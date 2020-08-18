a5c6fcc502145e559ef55935afc9b4390e9a9d3e
hinohara <hinoshin0817@gmail.com>
2020-08-18 11:43:07 +0900

    add function

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,15 +1,15 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom'
 
 
 const Header = (props) => {
   return <h1>{props.course}</h1>
 }
-const Button = () => (
+const Button = ({plusGood, plusNeutral, plusBad}) => (
   <div>
-    <button>good</button>
-    <button>neutral</button>
-    <button>bad</button>
+    <button onClick={plusGood}>good</button>
+    <button onClick={plusNeutral}>neutral</button>
+    <button onClick={plusBad}>bad</button>
   </div>
 )
 
@@ -20,10 +20,10 @@ const Result = (props) =>(
     <p>bad{props.bad}</p>
   </div>
 )
-const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+const App = (props) => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
   const course = {
     name: 'Half Stack application development',
     parts: [
@@ -41,13 +41,21 @@ const App = () => {
       }
     ]
   }
-
+  const plusGood = () => {
+    setGood(good+1)
+  }
+  const plusNeutral = () => {
+    setNeutral(neutral+1)
+  }
+  const plusBad = () => {
+    setBad(bad+1)
+  }
   return (
     <div>
       <h2>give feedback</h2>
-      <Button good={good} bad={bad} neutral={neutral}/>
+      <Button plusGood={plusGood} plusNeutral={plusNeutral} plusBad={plusBad}/>
       <h2>statisctic</h2>
-      <Result />
+      <Result good={good} bad={bad} neutral={neutral} />
     </div>
   )
 }
